37b5dbf8dad29e9b298756549f9ccf6b952ea630
Xiaoyu <xxie66@uwo.ca>
2019-11-11 18:54:24 -0500

    step1

---
+++ assn3.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>xxie66assignment3</title>
+</head>
+<body>
+<?php 
+include 'connectdb.php';
+ ?>
+	<h1>Welcome to Xiaoyu's Assignment3!</h1>
+	<h2>Doctor Info</h2>
+	<table>
+		<thead>
+			<tr>
+				<th>First Name<br>
+					<a href="getdata.php?order=fname_asc">
+						<img src="arrow_down.png" alt="Sort from A to Z">
+					</a>
+					<a href="getdata.php?order=fname_dsc">
+						<img src="arrow_up.png" alt="Sort from Z to A">
+					</a>
+				</th>
+				<th>Last Name<br>
+					<a href="getdata.php?order=lname_asc">
+						<img src="arrow_down.png" alt="Sort from A to Z">
+					</a>
+					<a href="getdata.php?order=lname_dsc">
+						<img src="arrow_up.png" alt="Sort from Z to A">
+					</a>
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+		</tbody>
+	</table>
+	<a href="getdata.php?order=fname_asc"></a>
+	
+</body>
+</html>
---
+++ getdata.php
@@ -0,0 +1,25 @@
+<?php 
+$query = "SELECT * FROM doctor, hospital WHERE doctor.hospitalcode=hospital.hospitalcode ORDER BY ";
+$order=$_GET['order'];
+if ($order == 'fname_asc'){
+	$query .='fname ASC';
+}else if ($order == 'fname_dsc'){
+	$query .='fname DESC';
+}else if ($order == 'lname_asc'){
+	$query .='lname ASC';
+}else if ($order == 'lname_dsc'){
+	$query .='lname DESC';
+}
+
+$result = mysqli_query($connect,$query);
+if(!result){
+	die("query unsuccessful");
+}
+echo "<ol>";
+while($row = mysqli_fetch_assoc($result)){
+	echo "<li>";
+	echo $row["fname"];
+	echo '">'.$row["fname"]." " .$row["lname"]."<br>";
+}
+mysqli_free_result($result);
+ ?>
