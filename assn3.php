2ca6aa270308bc2e65508f906a644b9a81101759
Xiaoyu <xxie66@uwo.ca>
2019-11-11 21:56:26 -0500

    change

--- assn3.php
+++ assn3.php
@@ -6,12 +6,7 @@
 </head>
 <body>
 <?php
-$host = "localhost";
-$user = "root";
-$passwd = "yh19981021";
-$dbname = "xxie66assign2db";
-
-$connect = mysqli_connect( $host, $user, $passwd, $dbname );
+include 'connectdb.php';
 ?>
 <h1>Welcome to Xiaoyu's Assignment3!</h1>
 <h2>Doctor Info</h2>
@@ -27,31 +22,14 @@ $connect = mysqli_connect( $host, $user, $passwd, $dbname );
   <tbody>
   </tbody>
 </table>
+<form action="getmoreinfo.php" method="post">
 <?php
-
-$query = "SELECT * FROM doctor, hospital WHERE doctor.hospitalcode=hospital.hospitalcode ORDER BY ";
-$order = $_GET[ 'order' ];
-if ( $order == 'fname_asc' ) {
-  $query .= 'fname ASC';
-} else if ( $order == 'fname_dsc' ) {
-  $query .= 'fname DESC';
-} else if ( $order == 'lname_asc' ) {
-  $query .= 'lname ASC';
-} else if ( $order == 'lname_dsc' ) {
-  $query .= 'lname DESC';
-}
-
-$result = mysqli_query( $connect, $query );
-if ( !result ) {
-  die( "query unsuccessful" );
-}
-echo "<ol>";
-while ( $row = mysqli_fetch_assoc( $result ) ) {
-  echo "<li>";
-  echo $row[ "fname" ];
-  echo '>' . $row[ "lname" ] . "<br>";
-}
-mysqli_free_result( $result );
+	include 'getdata.php';
+?>
+<input type="submit" value="Get More Info">
+</form>
+<?php 
+mysqli_close($connect);
 ?>
 
 </body>
