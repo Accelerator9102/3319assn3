0311cc209650367176ef619fef7419bddca4da44
Xiaoyu <xxie66@uwo.ca>
2019-11-11 23:41:04 -0500

    change

--- assn3.php
+++ assn3.php
@@ -30,15 +30,19 @@ include 'connectdb.php';
 </form>
 
 <form name="form1" action="assn3.php" method="post">
-	Select date: <input type="date" name="d" value=""><br>
+	Select date: <input type="date" name="d" value="0000-00-00"><br>
 	<input type="submit" value="Submit!">
 </form>
 <?php 
+
 $date = $_POST['d'];
-$query1='SELECT * FROM doctor WHERE doctor.datelicensed<"'.$date.'"';
-$result1=mysqli_query($connect,$query1);
-while ( $row1 = mysqli_fetch_assoc( $result1 ) ) {
-  echo $row1["fname"]." ".$row1[ "lname" ]. " ".$row1["specialty"]." ".$row1["datelicensed"]."<br>";
+if ($date!="0000-00-00") {
+
+	$query1='SELECT * FROM doctor WHERE doctor.datelicensed<"'.$date.'"';
+	$result1=mysqli_query($connect,$query1);
+	while ( $row1 = mysqli_fetch_assoc( $result1 ) ) {
+  		echo $row1["fname"]." ".$row1[ "lname" ]. " ".$row1["specialty"]." ".$row1["datelicensed"]."<br>";
+}
 }
 mysqli_close($connect);
 ?>
