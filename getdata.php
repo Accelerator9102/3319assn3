455df7516e4ca64d7072d9912593a199e52d5f3f
Xiaoyu <xxie66@uwo.ca>
2019-11-11 19:19:43 -0500

    change

--- getdata.php
+++ getdata.php
@@ -26,7 +26,7 @@ echo "<ol>";
 while($row = mysqli_fetch_assoc($result)){
 	echo "<li>";
 	echo $row["fname"];
-	echo '">'.$row["fname"]." " .$row["lname"]."<br>";
+	echo '>'.$row["fname"]."<br>";
 }
 mysqli_free_result($result);
  ?>
