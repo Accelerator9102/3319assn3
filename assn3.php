2e2383dd2a269ffd380ce9e0d589ddc0922911e9
Xiaoyu <xxie66@uwo.ca>
2019-11-11 23:55:37 -0500

    change

--- assn3.php
+++ assn3.php
@@ -17,8 +17,8 @@ include 'connectdb.php';
         <a href="assn3.php?order=fname_asc"> <img src="arrow_down.png" width="25px" alt="Sort from A to Z"> </a> <a href="assn3.php?order=fname_dsc"> <img src="arrow_up.png" width="25px" alt="Sort from Z to A"> </a> </th><br>
       <th align="right">Last Name<br>
         <a href="assn3.php?order=lname_asc"> <img src="arrow_down.png" width="25px" alt="Sort from A to Z"> </a> <a href="assn3.php?order=lname_dsc"> <img src="arrow_up.png" width="25px" alt="Sort from Z to A"> </a> </th><br>
-    </tr>
-  </thead>
+    </tr><br>
+  </thead><br>
   <tbody>
   </tbody>
 </table><br>
