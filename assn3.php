6319f408c1fcd7ae07f047f06a42459d8ebdc6f7
Xiaoyu <xxie66@uwo.ca>
2019-11-13 17:43:05 -0500

    change

--- assn3.php
+++ assn3.php
@@ -102,11 +102,11 @@ $doctor_licensenum=$_POST["doctorlicensenum"];
 	if($existence==1){
 		echo "Doctor going to be deleted is currently treating a patient!"."<br>";
 		echo "continue?";
-		echo '<form action="assn3.php" method="get">
-		<input type="radio" name="deletedoc" value="yes">Of Course!
-		<input type="radio" name="deletedoc" value="no">Noooooo!
-		<input type="submit" value="Good to Go!">
-		</form>';
+		echo '"<form action='assn3.php' method='get'>
+		<input type='radio' name='deletedoc' value='yes'>Of Course!
+		<input type='radio' name='deletedoc' value='no'>Noooooo!
+		<input type='submit' value='Good to Go!'>
+		</form>"';
 		$decision=$_GET["deletedoc"];
 		echo $decision;
 		if($decision=="yes"){
