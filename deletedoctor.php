2c86f121d8ba57038ed2433f02f47f4c0eb60e32
Xiaoyu <xxie66@uwo.ca>
2019-11-13 22:11:33 -0500

    change

--- deletedoctor.php
+++ deletedoctor.php
@@ -7,7 +7,7 @@ include 'connectdb.php';
 		var_dump($decision);
 		echo $decision;
 		if(strcmp($decision,"yes")==0){
-			$query4="DELETE FROM doctor WHERE licensenum='".$doctor_licensenum."'";
+			$query4="DELETE FROM doctor WHERE licensenum='".($GLOBALS['doctor_licensenum'])."'";
 			echo $query4;
 			if(!mysqli_query($GLOBALS['connect'],$query4)){
 				die("Deletion failed".mysqli_error($GLOBALS['connect']));
