d8e66786e059fb979d8dad20b86ed5958fe98bed
Xiaoyu <xxie66@uwo.ca>
2019-11-13 20:07:07 -0500

    change

--- assn3.php
+++ assn3.php
@@ -79,7 +79,7 @@ if ($date!="") {
 
 <form action="assn3.php" method="get">
 
-<select name="doctorlicensenumber">
+<select name="doctorlicensenumber" disabled="disabled">
 	<option selected="selected" value="None">Select Doctor: </option>
 <?php 
 include 'getdoctorlist.php' ?>
