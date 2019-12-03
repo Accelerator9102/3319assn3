734e0e70d44979d299b4438a6ef546f1036b7ec6
Accelerator9102 <55956981+Accelerator9102@users.noreply.github.com>
2019-12-03 18:11:51 -0500

    updating

--- updatehospital.php
+++ updatehospital.php
@@ -13,17 +13,8 @@
 	 	<?php 
 	 		$hospitalname=$_POST["hospitalname"];
 	 		$hospitalcode=$_POST["hospitalcode"];
-	 		echo $doctor_hospital;
-	 		$query1='SELECT * FROM hospital';
-	 		$result=mysqli_query($connect,$query1);
-	 		if(!$result){
-	 			die("query failed");
-	 		}
-	 		while($row=mysqli_fetch_assoc($result)){
-	 			if($hospitalcode==$row["hospitalcode"]){
-	 				$hospitalname=$row["name"];
-	 			}
-	 		}
+
+	 		
 	 		
 	 		
              $query='UPDATE hospital SET name="'.$hospitalname.'" WHERE hospitalcode="'.$hospitalcode.'"';
