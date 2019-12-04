5c0153ad99bc62fd527f9b6fde9c6c1e268cc840
Accelerator9102 <55956981+Accelerator9102@users.noreply.github.com>
2019-12-03 19:09:58 -0500

    updating

---
+++ patientinfo.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<?php 
+		include 'connectdb.php';
+	 ?>
+	 <h1>Patient Info: </h1>
+	 <ol>
+     <?php
+	$query8='SELECT * FROM treats WHERE OHIPnumber="'.$_POST["ohipnumber"].'"';
+	$result8=mysqli_query($connect,$query8);
+	
+	while($row8=mysqli_fetch_assoc($result8)){
+		$licensen=$row8["licensenum"];
+		$query9 = 'SELECT * FROM doctor WHERE licensenum = "'.$licensen.'"';
+		$result9=mysqli_query($connect,$query9);
+
+		$ohipn=$row8["OHIPnumber"];
+		$query10 = 'SELECT * FROM patient WHERE OHIPnumber = "'.$ohipn.'"';
+		$result10=mysqli_query($connect,$query10);
+		if(!($row9=mysqli_fetch_assoc($result9))&&($row10=mysqli_fetch_assoc($result10))){
+			echo "Error"."<br>";
+		}
+		while(($row9=mysqli_fetch_assoc($result9))&&($row10=mysqli_fetch_assoc($result10))){
+		echo $row10["fname"]." ".$row10["lname"]." treated by ".$row9["fname"]." ".$row9["lname"]."<br>";	
+		}
+		
+		
+	} 
+	
+	 ?>
+	 </ol>
+</body>
+</html>
